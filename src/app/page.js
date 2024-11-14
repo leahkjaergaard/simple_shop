c26442c942c1a2cc0de5caa40dbe6dfd8707630f
Leah <lepa0001@stud.kea.dk>
2024-11-14 13:29:26 +0100

    layout og styling på forsiden

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="relative">
       <div className="relative w-full h-[730px]">
-        <Image src="https://images.unsplash.com/flagged/photo-1570733117311-d990c3816c47?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Nature landscape" layout="fill" objectFit="cover" />
-        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
+        <Image src="https://images.unsplash.com/photo-1556918589-eae612a2bbc2?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Nature landscape" layout="fill" objectFit="cover" />
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-0">
           <h1 className="text-white text-9xl font-bold mb-4">LEMIL</h1>
-          <Link href="/products" className="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-all duration-300">
+          <Link href="/products" className="bg-red-50 text-black px-6 py-2 rounded-full hover:bg-blue-100 transition-all duration-400">
             UDFORSK NU!
           </Link>
         </div>
